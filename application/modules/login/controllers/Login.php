f4677e4ba0955b6079834429f313271744df4bfe
I Kadek Adi Suandana <mr.kadoel@gmail.com>
2015-04-19 15:56:09 +0800

    Menambahkan Session Untuk Beberapa Data

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -9,7 +9,7 @@ class Login extends CI_Controller{
 		parent::__construct();
 		$this->load->library(array('form_validation', 'MY_Handle'));
 		$this->load->helper(array('form'));
-		$this->load->model(array('m_pengajar'));
+		$this->load->model(array('m_pengajar', 'm_setting'));
 		
 		if($this->session->userdata('logged_in')){
 			redirect(site_url('admin'));
@@ -36,12 +36,15 @@ class Login extends CI_Controller{
 			$cek_user = $this->m_pengajar->cek_login($username, $password)->row();
 			$numb_count = count($cek_user);
 			if($numb_count > 0){
+				$pengaturan = $this->m_setting->ambil_setting('1')->row();
 				//Setting Session
 				$array_session = array(
 					'user_id' => $cek_user->pengajar_id,
 					'user_nama' => $cek_user->pengajar_nama,
 					'user_login' => $cek_user->pengajar_username,
-					'group_user' => $cek_user->pengajar_group, 
+					'group_user' => $cek_user->pengajar_group,
+					'semester' => $pengaturan->setting_semester,
+					'tahunajaran' => $pengaturan->setting_thnajaran,
 					'logged_in' => TRUE
 					);
 				$this->session->set_userdata($array_session);
